d377ba1272dbcbf38620fc69cc4b02b28e152c23
boyer <asboyer@gmail.com>
2021-02-08 17:36:31 -0500

    automated commit --updating repository

--- module/boyer/_boyer.py
+++ module/boyer/_boyer.py
@@ -1,4 +1,31 @@
-__all__ = ['hello']
+import sys as _sys
+import time as _time
+
+__all__ = ['hello', 'delay_print']
 
 def hello(name):
 	print(f"Hello {name}, my name is Andrew Boyer")
+
+def delay_print(s, end=''):
+	for c in s:
+		_sys.stdout.write(c)
+		_sys.stdout.flush()
+		_time.sleep(0.04)
+	print(end)
+
+def get_int(prompt, start=0, finish=0):
+	# 
+	while True:
+		try:
+			x = int(input(f"{prompt}: "))
+			break
+		except ValueError:
+			print("Please enter an integer!")
+		if finish != 0: 
+			if start != 0:
+				if x < finish and x > start:
+					break
+			elif x < finish:
+				break
+			else:
+				print(f"Enter a value between {start} and {finsh}")
--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
-"""
-Setup file for reporty library
-"""
-
 from setuptools import setup
 
 DISTNAME = 'boyer'
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 
 DESCRIPTION = "Andrew Boyer's personnal python library"
 
@@ -14,20 +10,20 @@ LONG_DESCRIPTION = """
 **boyer** is a python library that contains useful 
 functions for Andrew Boyer
 
-
 Functions:
 * Hello
+* Delay Print
 
-More functions **coming soon**
+Get int is **coming soon**
 
 Report any bugs or email us with questions: andrewsboyer2@gmail.com
 """
 
 LICENSE = 'MIT'
 
-AUTHOR = 'Andrew Boyer'
+AUTHOR = "Andrew Boyer"
 
-EMAIL = 'andrewsboyer2@gmail.com'
+EMAIL = 'andrewsboyer@gmail.com'
 
 URL = 'https://github.com/asboyer2/boyer'
 
@@ -38,19 +34,17 @@ REQUIREMENTS = []
 PYTHON = ">=3.5"
 
 setup(name=DISTNAME,
-      packages=['boyer'],
-      package_dir={'boyer': 'module/boyer'},
-      # package_data={'reporty': ['templates/*.yaml']},
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      license=LICENSE,
-      author=AUTHOR,
-      author_email=EMAIL,
-      url=URL,
-      keywords=KEYWORDS,
-      install_requires=REQUIREMENTS,
-      python_requires=PYTHON,
-      include_package_data=True
-      )
+	packages=['boyer'],
+	package_dir={'boyer': 'module/boyer'},
+	version=VERSION,
+	description=DESCRIPTION,
+	long_description=LONG_DESCRIPTION,
+	long_description_content_type='text/markdown',
+	license=LICENSE,
+	author=AUTHOR,
+	author_email=EMAIL,
+	url=URL,
+	keywords=KEYWORDS,
+	install_requires=REQUIREMENTS,
+	python_requires=PYTHON,
+	)
